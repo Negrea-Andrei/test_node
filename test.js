80ab945b55c784d07523191a0ba7c8c2099208a5
Negrea-Andrei <negrea97@yahoo.com>
2023-12-09 21:00:29 +0200

    tested the URL methods

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-let fs = require('fs');
+const myURL = new URL('/foo', 'https://example.org/');
 
-fs.rename('b.json', 'bbbbbbbbbbb.json', err => {
-    if(err) {
-        return console.log("error")
-    }
-})
+myURL.password = "123"
+myURL.username = "ala"
 
+if(myURL.password === "123" && myURL.username === "ala") {
+    console.log("Welcome")
+}
+else {
+    console.log('Error')
+}
